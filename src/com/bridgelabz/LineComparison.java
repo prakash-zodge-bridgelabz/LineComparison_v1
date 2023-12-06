b313f5220942e372f3c9b68afba839037d797a8f
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-06 22:22:26 +0530

    First commit

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison");
+    }
+    
+}
